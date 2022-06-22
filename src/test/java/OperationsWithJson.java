25e869fe3be26c3ecf635a51e33c021de83f3716
Loarlam <Loarlam@ya.ru>
2022-06-23 01:53:18 +0300

    Добавил public для static методов в Book.java. Добавил проверку полей в OperationsWithJson.java.

--- src/test/java/OperationsWithJson.java
+++ src/test/java/OperationsWithJson.java
@@ -6,6 +6,8 @@ import pojoForJSON.Book;
 import java.io.File;
 import java.io.IOException;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class OperationsWithJson {
     private String pathToJson = "src/test/resources/json/Book.json";
 
@@ -14,6 +16,10 @@ public class OperationsWithJson {
     void deserializingJson() throws IOException {
         ObjectMapper objectMapper = new ObjectMapper();
         Book book = objectMapper.readValue(new File(pathToJson), Book.class);
-        System.out.println(book);
+        assertThat(book.getTitle()).isEqualTo("The Cuckoo's Calling");
+        assertThat(book.getAuthor()).isEqualTo("Robert Galbraith");
+        assertThat(book.getGenre()).isEqualTo("classic crime novel");
+        assertThat(book.getDetail().getIsbn()).isNotNull();
+        assertThat(book.getPrice().get(1).getCost()).isGreaterThan(5);
     }
 }
